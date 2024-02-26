3769f8b2078fbc984fa782d69f17bc7db5cc77b8
Pepa Labrin <catu.leibrin@gmail.com>
2024-02-26 16:49:35 -0300

    added ItemListContainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import Navbar from './components/Navbar/Navbar'
-import CartWidget from './components/CartWidget/CartWidget'
 import './App.css'
+import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 
 function App() {
 
   return (
     <>
     <Navbar />
+      <ItemListContainer greeting={'¡Bienvenidos a Café Triciclo!'} />
 
     </>
   )
